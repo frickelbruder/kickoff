a2d89cd868f0a058382a655c0898a2df1a4b4c41
Markus <mah@netvisio.de>
2016-09-05 01:06:06 +0200

    HttpRuleBase - GetHeader normalize

--- src/Rules/HttpRuleBase.php
+++ src/Rules/HttpRuleBase.php
@@ -5,11 +5,14 @@ use Frickelbruder\KickOff\Rules\Exceptions\HeaderNotFoundException;
 
 abstract class HttpRuleBase extends RuleBase {
 
-    protected function findHeader($headerName) {
+    protected function findHeader($headerName, $normalize = true) {
         $loweredHeaderName = strtolower($headerName);
         $headers = $this->httpResponse->getHeaders();
         foreach($headers as $key => $header) {
             if(strtolower($key) == $loweredHeaderName) {
+                if($normalize && is_array($header) ) {
+                    return implode("\n", $header);
+                }
                 return $header;
             }
         }
