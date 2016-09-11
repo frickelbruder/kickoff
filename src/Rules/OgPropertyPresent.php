e83f2dc8514b074d00894c8ef1427211500351e6
Markus <mah@netvisio.de>
2016-09-11 16:13:06 +0200

    new OGPropertyPresent Rule

---
+++ src/Rules/OgPropertyPresent.php
@@ -0,0 +1,41 @@
+<?php
+namespace Frickelbruder\KickOff\Rules;
+
+class OgPropertyPresent extends ConfigurableRuleBase  {
+
+    public $name = 'OgPropertyPresent';
+
+    protected $errorMessage = 'The required open graph property is not present.';
+
+    protected $requiredProperties = array('title', 'type', 'url', 'image');
+
+    protected $configurableField = array('requiredProperties');
+
+    public function validate() {
+        try {
+            $body = $this->httpResponse->getBody();
+            $xml = $this->getResponseBodyAsXml( $body );
+
+            foreach( $this->requiredProperties as $propertyName ) {
+                $propertyItemValue = $xml->xpath( '/html/head/meta[@property="og:' . $propertyName . '"]/ @content' );
+                if( !is_array( $propertyItemValue ) || empty( $propertyItemValue ) ) {
+                    $this->errorMessage = 'The open graph property "' . $propertyName . '" was not found on the site.';
+
+                    return false;
+                }
+                $length = mb_strlen( $propertyItemValue[0]['content'], 'UTF-8' );
+
+                if( $length == 0 ) {
+                    $this->errorMessage = 'The open graph property "' . $propertyName . '" was found but is empty.';
+
+                    return false;
+                }
+            }
+
+            return true;
+        } catch(\Exception $e) {
+            $this->errorMessage = $e->getMessage();
+        }
+        return false;
+    }
+}
---
+++ tests/Rules/OgPropertyPresentTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Frickelbruder\Kickoff\Tests\Rules;
+
+use Frickelbruder\KickOff\Http\HttpResponse;
+use Frickelbruder\KickOff\Rules\OgPropertyPresent;
+
+class OgPropertyPresentTest extends \PHPUnit_Framework_TestCase {
+
+    private $testString = '<!DOCTYPE html><html><head><meta property="og:title" content="Og Title"><meta property="og:description" content="Og Description"><meta property="og:image" content=""></head></html>';
+
+
+    public function testValidate() {
+        $response = new HttpResponse();
+        $response->setBody( $this->testString );
+
+        $rule = new OgPropertyPresent();
+        $rule->setHttpResponse( $response );
+        $rule->set( 'requiredProperties', array( 'title', 'description' ) );
+
+        $result = $rule->validate();
+        $this->assertTrue( $result );
+    }
+
+    public function testValidateWithEmptyPropertyIsFalse() {
+        $response = new HttpResponse();
+        $response->setBody( $this->testString );
+
+        $rule = new OgPropertyPresent();
+        $rule->setHttpResponse( $response );
+        $rule->set( 'requiredProperties', array( 'title', 'description', 'image' ) );
+
+        $result = $rule->validate();
+        $this->assertFalse( $result );
+    }
+
+    public function testValidateWithNotFoundPropertyIsFalse() {
+        $response = new HttpResponse();
+        $response->setBody( $this->testString );
+
+        $rule = new OgPropertyPresent();
+        $rule->setHttpResponse( $response );
+        $rule->set( 'requiredProperties', array( 'title', 'description', 'image:secure_url' ) );
+
+        $result = $rule->validate();
+        $this->assertFalse( $result );
+    }
+
+}
