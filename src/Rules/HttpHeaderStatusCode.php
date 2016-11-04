e1e1f8d1096f514ed97e66b8c8c5013c5a99601a
Markus <mah@netvisio.de>
2016-11-04 12:05:22 +0100

    HttpHeaderStatusCode configurable

--- src/Rules/HttpHeaderStatusCode.php
+++ src/Rules/HttpHeaderStatusCode.php
@@ -2,13 +2,15 @@
 namespace Frickelbruder\KickOff\Rules;
 
 
-class HttpHeaderStatusCode extends RuleBase {
+class HttpHeaderStatusCode extends ConfigurableRuleBase  {
 
     public $name = "Http header: Status code";
 
     protected $errorMessage = 'The resources HTTP status code was unexpected.';
 
     protected $value = 200;
+
+    protected $configurableField = array('value');
 
     public function validate() {
         $resultCode = $this->httpResponse->getStatus();
